22231c98a438789a48de353c3be92473b1de48bc
Miha Vintar <mv74130@student.uni-lj.si>
2023-02-23 15:17:19 +0100

    Dodan izracun pospeska

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,5 +1,13 @@
-public class Gravitacija{
-    public static void main(String[] args){
+public class Gravitacija {
+
+    public static void main(String[] args) {
         System.out.println("OIS je zakon!");
+    }
+
+    public static double g(double v) {
+        final double C = 6.674 * Math.pow(10, -11);
+        final double M = 5.972 * Math.pow(10, 24);
+        final double r = 6.371 * Math.pow(10, 6);
+        return C * M / Math.pow((r + v), 2);
     }
 }
